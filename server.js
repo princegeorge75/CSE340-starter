c5a7624faaa5e18512a1393301a6631d8813856a
princegeorge75 <princegeorgexyx@gmal.com>
2026-04-06 16:34:07 +0300

    Removed purser - W04

--- server.js
+++ server.js
@@ -7,7 +7,6 @@
 /* ***********************
  * Require Statements
  *************************/
-const bodyParser = require("body-parser")
 const accountRoute = require("./routes/accountRoute")
 const session = require("express-session")
 const pool = require('./database/')
